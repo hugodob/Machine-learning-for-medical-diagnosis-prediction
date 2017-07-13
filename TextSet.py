ad057091cacc4d484c2ae28febd3e7dc58eb9dbe
hugodob <hugo.dobbelaere@polytechnique.edu>
2017-07-13 12:14:09 -0700

    i

--- TextSet.py
+++ TextSet.py
@@ -7,16 +7,24 @@ import re
 csv.field_size_limit(500 * 1024 * 1024)
 
 class TextSet(object):
-    def __init__(self, headers, data):
+    def __init__(self, headers, text):
         self.headers=headers
-        self.data=data
-
+        self.text=text
+#import data from a csv file
     def import_from_csv(self, path_to_csv):
         with open(path_to_csv, 'r') as f:
             reader = csv.reader((line.replace('||', '|') for line in f), delimiter='|')
-            data = list(reader)
-            headers=data.pop(0)
-            data=np.array(data)
+            text = list(reader)
+            headers=text.pop(0)
+            text=np.array(text)
         self.headers=headers
-        self.data=data
-        return headers, data
+        self.text=text
+        return headers, text
+
+    def format_text(self):
+        for i in range(self.text.shape[0]):
+            self.text[i][1]=self.text[i][1].lower()
+            self.text[i][1]= re.sub(r'[^\w\s]','',self.text[i][1])
+        #Important to shuffle now if we want to have consistent results
+        np.random.shuffle(self.text)
+        return self.text
