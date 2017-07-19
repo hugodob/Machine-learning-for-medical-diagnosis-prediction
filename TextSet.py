a00a800bd22cdf59f07340fd31563dc5f99d7d67
hugodob <hugo.dobbelaere@polytechnique.edu>
2017-07-19 16:08:12 -0700

    Ajout du fichier TfidfEmbeddingVectorizer.py pour le prétraitement

--- TextSet.py
+++ TextSet.py
@@ -1,34 +1,30 @@
 import csv
 import sys
+import pandas as pd
 from collections import Counter
 import numpy as np
 import re
 from gensim import utils
 from gensim.models.doc2vec import LabeledSentence
-csv.field_size_limit(500 * 1024 * 1024)
 
-class TextSet(object):
-    def __init__(self, headers, text):
-        self.headers=headers
-        self.text=text
+
 #import data from a csv file
-    def import_from_csv(self, path_to_csv):
-        with open(path_to_csv, 'r') as f:
-            reader = csv.reader((line.replace('||', '|') for line in f), delimiter='|')
-            text = list(reader)
-            headers=text.pop(0)
-            text=np.array(text)
-        self.headers=headers
-        self.text=text
-        return headers, text
+def import_from_csv(path_to_folder):
+    train_data=pd.read_csv(path_to_folder+'/training_variants')
+    train_text=pd.read_csv(path_to_folder+'/training_text', sep="\|\|", engine='python', header=None, skiprows=1, names=["ID","Text"])
+    test_data=pd.read_csv(path_to_folder+'/test_variants')
+    test_text=pd.read_csv(path_to_folder+'/test_text', sep="\|\|", engine='python', header=None, skiprows=1, names=["ID","Text"])
+    train = pd.merge(train_data, train_text, how='left', on='ID').fillna('')
+    test = pd.merge(test_data, test_text, how='left', on='ID').fillna('')
+    return train, test
 
-    def format_text(self):
-        for i in range(self.text.shape[0]):
-            self.text[i][1]=self.text[i][1].lower()
-            self.text[i][1]= re.sub(r'[^\w\s]','',self.text[i][1])
-        #Important to shuffle now if we want to have consistent results
-        np.random.shuffle(self.text)
-        return self.text
+def format_text(data_frame):
+    for i in range(self.text.shape[0]):
+        self.text[i][1]=self.text[i][1].lower()
+        self.text[i][1]= re.sub(r'[^\w\s]','',self.text[i][1])
+    #Important to shuffle now if we want to have consistent results
+    np.random.shuffle(self.text)
+    return self.text
 
     def format_labeled_sentences(self):
         sentences=[]
